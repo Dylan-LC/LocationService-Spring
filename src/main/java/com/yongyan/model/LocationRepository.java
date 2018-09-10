1b660f4d3b0518c4ddc6610f2cf5ab9ebd10d983
Yongyan Liu <yongyanliu17@gmail.com>
2018-09-10 04:41:15 -0500

    build LocationRepository interface extanding existed database API to sharp data to satisfy the requirements of controller

---
+++ src/main/java/com/yongyan/model/LocationRepository.java
@@ -0,0 +1,18 @@
+package com.yongyan.model;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+
+/**
+ * created by yongyan on 09/10/2018
+ * combine database api with re-properties we build to satisfy the requirements of controller, break the middle wall.
+ */
+
+public interface LocationRepository extends JpaRepository<Location, Long> { // support what operations controller needs
+
+    Page<Location> findByVehicleMovementType(@Param("movementType") Location.VehicleMovementType movementType, Pageable pageable);
+    Page<Location> findByUnitInfoUnitVin(@Param("unitVin") String unitVin, Pageable pageable); // pageable para defines how to Page the output
+
+}
